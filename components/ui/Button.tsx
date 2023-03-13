93fed42dc97b8fee821ce551a71a5ef86c3c34eb
brhmty <ibrahimmatay@gmail.com>
2023-03-13 22:39:59 +0300

    bug fixed

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -12,10 +12,7 @@ function Button(props: { itemId: string }) {
         href={`/events/${itemId}`}
         className="md:w-max w-fit inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal flex bg-green-400 dark:bg-primary text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] whitespace-nowrap"
       >
-        <span className="lg:text-md md:text-lg text-[8px]">
-          {" "}
-          Explore Event{" "}
-        </span>
+        <span className="lg:text-xs md:text-lg text-[8px]">Explore Event</span>
         <span className="md:ml-2 ml-1">
           <ArrowRight />
         </span>
